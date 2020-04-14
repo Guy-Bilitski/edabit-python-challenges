6686225ca1002f326d1cdb1bc050a50d0064fb17
Guy-Bilitski <52350822+Guy-Bilitski@users.noreply.github.com>
2020-04-14 13:11:31 +0300

    corrected reduce with map

---
+++ very_hard_q9_pythagorean_triplet.py
@@ -0,0 +1,24 @@
+"""
+Create a function which validates whether 3 given integers form a Pythagorean triplet. The sum of the squares of the two smallest integers must equal the square of the largest number to be validated.
+
+Examples
+is_triplet(3, 4, 5) ➞ True
+# 3² + 4² = 25
+# 5² = 25
+
+is_triplet(13, 5, 12) ➞ True
+# 5² + 12² = 169
+# 13² = 169
+
+is_triplet(1, 2, 3) ➞ False
+# 1² + 2² = 5
+# 3² = 9
+Notes
+Numbers may not be given in a sorted order.
+"""
+from functools import reduce
+
+
+def is_triplet(a, b, c):
+    return max(a, b, c) ** 2 == reduce(lambda x, y: x + y, map(lambda x: x**2,
+                                                               filter(lambda d: d != max(a, b, c), [a, b, c])))
